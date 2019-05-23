2dffb265ee8df7d52b578ddf4fc26890a18f7b09
AnupamaRudra <anupamarudra111@gmail.com>
2019-05-23 11:00:59 +0530

    Update Jenkinsfile2.groovy

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -14,7 +14,7 @@ pipeline{
  		        stage('Two')
 			             {
                                      steps{
-                                            build 'decpipe1'
+                                            build 'first'
                                             }
 				           }
 
